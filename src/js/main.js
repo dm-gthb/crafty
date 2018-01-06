dfcd210990ff28b4795561849d6897b81065b482
Dmitry Mamonov <dmitry.mamonov@dataart.com>
2018-01-06 16:55:14 +0300

    add projects js & styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Header toggler
 var toggler = document.getElementById('toggler');
 var pageNav = document.querySelector('.page-nav');
 
@@ -9,11 +10,12 @@ function mobileNavVisibleToggle(event) {
   pageNav.classList.toggle('page-nav--active');
 }
 
-
 if (toggler) {
   toggler.addEventListener('click', mobileNavVisibleToggle);
 }
 
+
+// Header animation
 var tl = new TimelineMax();
 
 tl
@@ -24,6 +26,7 @@ tl
   .from('.btn--learn-more', 1, {opacity:0, scale:0, ease:Bounce.easeOut});
 
 
+// Header 'learn more' button
 var learnMoreBtn = document.querySelector('.btn--learn-more');
 var pageHeader = document.querySelector('.page-header');
 const viewportHeight = pageHeader.offsetHeight;
@@ -32,3 +35,42 @@ learnMoreBtn.addEventListener('click', function(event) {
   event.preventDefault();
   window.scrollBy({ top: viewportHeight, behavior: 'smooth' });
 });
+
+
+// Projects navigation
+var controls = document.querySelectorAll('.projects__nav-link');
+var projects = document.querySelectorAll('.projects__item');
+
+for (var i = 0; i < controls.length; i++) {
+   clickControl(controls[i]);
+}
+
+function toggleFilter(control) {
+
+  for (var k = 0; k < controls.length; k++) {
+    controls[k].classList.remove('projects__nav-link--active')
+  }
+
+  for (var l = 0; l < projects.length; l++) {
+    projects[l].classList.remove('projects__item--active')
+  }
+
+  control.classList.add('projects__nav-link--active');
+
+  var clickedTypeProjects = document.querySelectorAll('a.' + control.dataset.filter);
+  if (clickedTypeProjects) {
+    for (var m = 0; m < clickedTypeProjects.length; m++) {
+      clickedTypeProjects[m].classList.add('projects__item--active');
+    }
+  }
+}
+
+function clickControl(control) {
+  control.addEventListener('click', function() {
+    event.preventDefault();
+    toggleFilter(control);
+  });
+}
+
+var defaultFilter = document.querySelector('button.branding');
+toggleFilter(defaultFilter);
